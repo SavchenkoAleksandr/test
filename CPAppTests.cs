2ac65c7a40a220e1284f7698b54e8eaed1773214
SavchenkoAleksandr <102680552+SavchenkoAleksandr@users.noreply.github.com>
2022-03-30 18:12:02 +0300

    first commit test
    1

---
+++ CPAppTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Vision.QA.Automation.PlanPages;
+
+namespace Vision.FuncTests.CPApp 
+{
+    [TestFixture]
+    class CPAppTests : VisionTestBase
+    {
+        [Test]
+        public void createScenario()
+        {
+            ChannelPlanningPage cPPage = new ChannelPlanningPage();
+            var originalName = "Auto plan " + DateTime.Now;
+            ChannelPlanningPage.AddChannelPlanning(originalName).Execute();
+        }
+    }
+}
